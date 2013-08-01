a989fd7115b83b07cbeb6bea69fa79618b2c63b5
Ethan Zimmerman <thebinarypenguin@gmail.com>
2013-07-31 23:46:05 -0400

    Add session-based auth to socket.io requests

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var express   = require('express')
 
 app = express();
 
-var server = http.createServer(app);
-var io     = socket.listen(server);
+var server = http.createServer(app)
+  , io     = socket.listen(server);
+
+var cookieParser = express.cookieParser('your secret here')
+  , sessionStore = new express.session.MemoryStore();
 
 // Application Settings
 app.set('port', process.env.PORT || 3000);
@@ -22,8 +25,8 @@ app.locals.games = new GameStore();
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+app.use(cookieParser);
+app.use(express.session({ store: sessionStore }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
@@ -40,25 +43,49 @@ app.all('*',         routes.invalid);
 
 io.configure('development', function() {
   io.set('log level', 1);
+  io.set('authorization', function (handshakeData, callback) {
+    // Use the cookie parser middleware to parse the signed cookie and
+    // add that data to the handshakeData object as a signedCookies property
+    cookieParser(handshakeData, {}, function(err) {
+      if (err) return callback(err);
+      // Use that signed cookie data to load up the session
+      sessionStore.load(handshakeData.signedCookies['connect.sid'], function(err, session) {
+        if (err) return callback(err);
+        // Save session to handshakeData for use later in the event handlers
+        handshakeData.session = session;
+        callback(null, true);
+      });
+    });
+  });
 });
 
 // Sockets
 io.sockets.on('connection', function (socket) {
-
   console.log('Socket '+socket.id+' connected');
 
+  var sess = socket.handshake.session;
+
   socket.on('join', function(data) {
-    this.join(data.gameID);
+    if (data.gameID !== sess.gameID) {
+      console.log('ERROR: Game ID mismatch');
+      return;
+    }
 
     var game = app.locals.games.find(data.gameID);
     game.addPlayer(data);
 
     console.log(data.playerName+ ' joined '+data.gameID);
 
+    socket.join(data.gameID);
     io.sockets.in(data.gameID).emit('update', game);
   });
 
   socket.on('move', function(data) {
+    if (data.gameID !== sess.gameID) {
+      console.log('ERROR: Game ID mismatch');
+      return;
+    }
+
     var game = app.locals.games.find(data.gameID);
     game.move(data.move);
 
