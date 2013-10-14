6ef9b58cfa9199f16c6ed6151aa23d6f84e1cba2
Ethan Zimmerman <thebinarypenguin@gmail.com>
2013-10-14 11:46:14 -0400

    Make socket server emit error events when apropos

--- socketRoutes.js
+++ socketRoutes.js
@@ -9,46 +9,115 @@ exports.attach = function(io, DB) {
 
     var sess = socket.handshake.session;
 
-    // Join game
+    /*
+     * Join game
+     */
     socket.on('join', function(gameID) {
+
+      var debugInfo = {
+        socketID : socket.id,
+        event    : 'join',
+        gameID   : gameID,
+        session  : sess
+      };
+
+      // Check for permission
       if (gameID !== sess.gameID) {
-        console.log('ERROR: Game ID mismatch');
+        console.log('ERROR: Access Denied', debugInfo);
+        socket.emit('error', {message: "You cannot join this game"});
         return;
       }
 
+      // Look for game
       var game = DB.find(gameID);
-      if (game.addPlayer(sess)) {
-        console.log(sess.playerName+ ' joined '+gameID);
-        socket.join(gameID);
-        io.sockets.in(gameID).emit('update', game);
-      } else {
-        console.log(sess.playerName+' failed to join '+gameID);
+      if (!game) {
+        console.log('ERROR: Game Not Found', debugInfo);
+        socket.emit('error', {message: "Game not found"});
+        return;
       }
+
+      // Add player to game
+      var result = game.addPlayer(sess);
+      if (!result) {
+        console.log('ERROR: Failed to Add Player', debugInfo);
+        socket.emit('error', {message: "Unable to join game"});
+        return;
+      }
+
+      // Add player to a socket.io "room" that matches the game ID
+      socket.join(gameID);
+
+      console.log(sess.playerName+' joined '+gameID);
+      io.sockets.in(gameID).emit('update', game);
     });
 
-    // Make a move
+    /*
+     * Make a move
+     */
     socket.on('move', function(data) {
+
+      var debugInfo = {
+        socketID : socket.id,
+        event    : 'move',
+        gameID   : data.gameID,
+        move     : data.move,
+        session  : sess
+      };
+
+      // Check for permission
       if (data.gameID !== sess.gameID) {
-        console.log('ERROR: Game ID mismatch');
+        console.log('ERROR: Access Denied', debugInfo);
+        socket.emit('error', {message: "You have not joined this game"});
         return;
       }
 
+      // Look for game
       var game = DB.find(data.gameID);
-      if (game.move(data.move)) {
-        console.log(sess.playerName+': '+data.move);
-        io.sockets.in(data.gameID).emit('update', game)
-      } else {
-        console.log(sess.playerName+': '+data.move+' Failed');
+      if (!game) {
+        console.log('ERROR: Game Not Found', debugInfo);
+        socket.emit('error', {message: "Game not found"});
+        return;
       }
+
+      // Apply move to game
+      var result = game.move(data.move);
+      if (!result) {
+        console.log('ERROR: Failed to Apply Move', debugInfo);
+        socket.emit('error', {message: "Invalid move, please try again"});
+        return;
+      }
+
+      console.log(data.gameID+' '+sess.playerName+': '+data.move);
+      io.sockets.in(data.gameID).emit('update', game);
     })
 
-    // Socket connection lost
+    /*
+     * Socket connection lost
+     */
     socket.on('disconnect', function() {
+
+      var debugInfo = {
+        socketID : socket.id,
+        event    : 'disconnect',
+        session  : sess
+      };
+
+      // Look for game
       var game = DB.find(sess.gameID);
-      if (game.removePlayer(sess)) {
-        console.log(sess.playerName+' left '+sess.gameID);
-        console.log('Socket '+socket.id+' disconnected');
+      if (!game) {
+        console.log('ERROR: Game Not Found', debugInfo);
+        return;
       }
+
+      // Remove player from game
+      var result = game.removePlayer(sess);
+      if (!result) {
+        console.log('ERROR: '+sess.playerName+' failed to leave '+sess.gameID);
+        return;
+      }
+
+      console.log(sess.playerName+' left '+sess.gameID);
+      console.log('Socket '+socket.id+' disconnected');
     });
   });
 };
