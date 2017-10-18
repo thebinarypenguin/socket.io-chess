dbd581f61389cf3320dce47c2885d1692e537468
snvital <snvital3812@gmail.com>
2017-10-18 08:42:57 -0400

    change application name

--- public/js/client.js
+++ public/js/client.js
@@ -64,17 +64,17 @@ var Client = (function(window) {
    * Assign square IDs and labels based on player's perspective
    */
   var assignSquares = function() {
-    var fileLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    var fileLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     var rankLabels = [8, 7, 6, 5, 4, 3, 2, 1];
     var squareIDs  = [
-      'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8', 'I8','J8',
-      'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'I7','J7'
-      'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6', 'I6','J6'
-      'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5', 'I5','J5'
-      'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4', 'I4','J4'
-      'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3', 'I3','J3'
-      'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2', 'I2','J2'
-      'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1', 'I1', 'J1'
+      'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8', 
+      'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 
+      'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6', 
+      'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5', 
+      'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4', 
+      'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3', 
+      'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2', 
+      'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'
     ];
 
     if (playerColor === 'black') {
